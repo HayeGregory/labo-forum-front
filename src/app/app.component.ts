63d6fe2f4b1323885625bb71eafdc0814f1fde4f
HayeGregory <haye.greg@outlook.com>
2020-12-16 13:54:50 +0100

    Step 1 : Mise en place design - js via view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  @ViewChild("collapsible", {static: false}) eRef: ElementRef<HTMLUListElement>;
+  
   title = 'Forum-Front';
+  mCollapsibleInstance: M.Collapsible;
+
+  ngAfterViewInit() {
+    this.mCollapsibleInstance = M.Collapsible.init(this.eRef.nativeElement);
+  }
 }
